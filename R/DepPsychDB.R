38b8848d3818f6e376617ccab954d949140d7adb
Mathias Harrer <mathias.h.harrer@gmail.com>
2020-06-01 00:24:00 +0200

    first commit

---
+++ R/DepPsychDB.R
@@ -0,0 +1,10 @@
+#' Meta-Analytic Depression Psychotherapy Database Dataset
+#'
+#' @source Meta-Analytic Depression Psychotherapy Database, maintained by VU
+#' Amsterdam Clinical Psychology and Psychotherapy.
+#'
+#' @usage data(DepPsychDB)
+#'
+#' @author Pim Cuijpers
+#'
+"DepPsychDB"
---
+++ R/open_app.R
@@ -0,0 +1,17 @@
+#' Open Meta-Analytic Depression Psychotherapy Database Web App.
+#'
+#' Opens the Meta-Analytic Depression Psychotherapy Database Web App online.
+#'
+#' @usage open_app()
+#'
+#' @author Mathias Harrer
+#'
+#' @importFrom utils browseURL
+#' @export open_app
+#'
+#' @examples
+#' open_app()
+
+open_app = function(){
+  utils::browseURL("https://protectlab.shinyapps.io/depression-psychotherapy-db/")
+}
