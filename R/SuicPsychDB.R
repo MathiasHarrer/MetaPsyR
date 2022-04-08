110f80cf2d99779a2e573f6f3b52d4b009cbb56e
Mathias Harrer <mathiasharrer@Mathiass-MBP.localdomain>
2022-04-08 13:33:29 +0200

    upload suicide data

---
+++ R/SuicPsychDB.R
@@ -0,0 +1,8 @@
+#' Meta-Analytic Suicide Prevention Database Dataset
+#'
+#' @source Meta-Analytic Suicide Prevention Database.
+#' @usage data(SuicPsychDB)
+#'
+#' @author Wouter van Ballegooijen, Josine Rawee
+#'
+"SuicPsychDB"
